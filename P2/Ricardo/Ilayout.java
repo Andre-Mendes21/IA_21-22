f2c813a9843d425070ac5b953f11dd5f05c79241
Andre Mendes <andre.f.mendes24@gmail.com>
2021-10-26 16:28:42 +0100

    Initial commit for second problem

---
+++ P2/Ricardo/Ilayout.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public interface Ilayout {
+    /**
+     * @return the children of the receiver
+     */
+    List<Ilayout> children();
+
+    /**
+     * @return true if the receiver equals the arguments 1; 
+     *         return false otherwise
+     */
+    boolean isGoal(Ilayout l);
+
+    /**
+     * @return the cost for moving from the input config to the receiver.
+    */
+    double getG();
+}
