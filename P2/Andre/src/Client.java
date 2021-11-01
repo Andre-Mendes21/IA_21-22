cd4671869c584952cdc6307845dfb9a969f2d458
Andre Mendes <andre.f.mendes24@gmail.com>
2021-11-01 22:48:06 +0000

    Added package src

--- P2/Andre/src/Client.java
+++ P2/Andre/src/Client.java
@@ -1,10 +1,14 @@
+package src;
+
 import java.util.*;
 
 public class Client {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         BestFirst s = new BestFirst();
-        Iterator<BestFirst.State> it = s.solve(new Board(sc.next()), new Board(sc.next()));
+        int[] jugCapacity = {8, 5, 3};
+
+        Iterator<BestFirst.State> it = s.solve(new Board(sc.nextLine(), jugCapacity), new Board(sc.nextLine(), jugCapacity));
 
         if(it == null) {
             System.out.println("no solution was found");
