e424297a3ec6157ab7bd6c8109e10b70357e6b26
Andre Mendes <andre.f.mendes24@gmail.com>
2021-11-14 11:48:48 +0000

    Refactoring of client

--- P3/src/Client.java
+++ P3/src/Client.java
@@ -2,20 +2,15 @@ import java.util.*;
 
 public class Client {
 
-    private static int[] getJugs(Scanner sc) {
-        int[] jugs = new int[3];
-
-        for(int i = 0; i < 3; i++) {
-            jugs[i] = sc.nextInt();
-        }
-        return jugs;
-    }
-
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         BestFirst s = new BestFirst();
 
-        Iterator<BestFirst.State> it = s.solve(new Jug(getJugs(sc)), new Jug(getJugs(sc)));
+        String jugsCapacity = sc.nextLine().trim();
+        String currJugs = sc.nextLine().trim();
+        String objective = sc.nextLine().trim();
+
+        Iterator<BestFirst.State> it = s.solve(new Jug(jugsCapacity, currJugs), new Jug(jugsCapacity, objective));
 
         if(it == null) {
             System.out.println("no solution was found");
