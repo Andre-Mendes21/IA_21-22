d3b25aef6baff96b2caf0b84be808a9739ed03ac
Andre Mendes <andre.f.mendes24@gmail.com>
2021-11-14 15:54:35 +0000

    Added A* unit tests

--- P3/src/JugUnitTests.java
+++ P3/src/JugUnitTests.java
@@ -41,7 +41,6 @@ public class JugUnitTests {
         BestFirst s = new BestFirst();
         Iterator<BestFirst.State> it = s.solve(new Jug(jugsCapacity, currJugs), new Jug(jugsCapacity, out));
 
-        int j = 0;
         while(it.hasNext()) {
             BestFirst.State i = it.next();
             if(!it.hasNext()) {
@@ -59,7 +58,6 @@ public class JugUnitTests {
         BestFirst s = new BestFirst();
         Iterator<BestFirst.State> it = s.solve(new Jug(jugsCapacity, currJugs), new Jug(jugsCapacity, out));
 
-        int j = 0;
         while(it.hasNext()) {
             BestFirst.State i = it.next();
             if(!it.hasNext()) {
@@ -77,9 +75,94 @@ public class JugUnitTests {
         BestFirst s = new BestFirst();
         Iterator<BestFirst.State> it = s.solve(new Jug(jugsCapacity, currJugs), new Jug(jugsCapacity, out));
 
-        int j = 0;
         while(it.hasNext()) {
             BestFirst.State i = it.next();
+            if(!it.hasNext()) {
+                assertEquals(String.format("%.0f", expected), String.format("%.0f", i.getG()));
+            }
+        }
+    }
+
+    /*============================== A* Tests ==============================*/
+
+    @Test
+    public void testAStarSolve() {
+        String jugsCapacity = "8 5 3";
+        String currJugs = "8 0 0";
+        String out = "0 5 3";
+        double expected = 2f;
+        AStar s = new AStar();
+        Iterator<AStar.State> it = s.solve(new Jug(jugsCapacity, currJugs), new Jug(jugsCapacity, out));
+
+        while(it.hasNext()) {
+            AStar.State i = it.next();
+            if(!it.hasNext()) {
+                assertEquals(String.format("%.0f", expected), String.format("%.0f", i.getG()));
+            }
+        }
+    }
+
+    @Test
+    public void testAStarSolve2() {
+        String jugsCapacity = "21 13 8 5 3";
+        String currJugs = "21 0 0 0 0";
+        String out = "6 4 4 4 3";
+        double expected = 13f;
+        AStar s = new AStar();
+        Iterator<AStar.State> it = s.solve(new Jug(jugsCapacity, currJugs), new Jug(jugsCapacity, out));
+
+        while(it.hasNext()) {
+            AStar.State i = it.next();
+            if(!it.hasNext()) {
+                assertEquals(String.format("%.0f", expected), String.format("%.0f", i.getG()));
+            }
+        }
+    }
+
+    @Test
+    public void testAStarSolve2Expanded() {
+        String jugsCapacity = "21 13 8 5 3";
+        String currJugs = "21 0 0 0 0";
+        String out = "6 4 4 4 3";
+
+        String[] expected = {
+            "21 0 0 0 0",
+            "16 0 0 5 0",
+            "16 0 0 2 3",
+            "16 0 3 2 0",
+            "3 13 3 2 0",
+            "3 10 3 2 3",
+            "3 10 6 2 0",
+            "3 10 6 0 2",
+            "3 10 1 5 2",
+            "3 10 1 4 3",
+            "3 10 4 4 0",
+            "3 7 4 4 3",
+            "6 7 4 4 0",
+            "6 4 4 4 3" };
+        
+
+        AStar s = new AStar();
+        Iterator<AStar.State> it = s.solve(new Jug(jugsCapacity, currJugs), new Jug(jugsCapacity, out));
+
+        int j = 0;
+        while(it.hasNext()) {
+            AStar.State i = it.next();
+            assertEquals(expected[j++], i.toString());
+        }
+    }
+
+    @Test
+    public void testAStarSolve3() {
+        String jugsCapacity = "8 7 6 5 4 3 2 1";
+        String currJugs = "0 0 0 0 4 3 2 1";
+        String out = "1 1 1 1 1 3 1 1";
+        double expected = 8f;
+        AStar s = new AStar();
+        Iterator<AStar.State> it = s.solve(new Jug(jugsCapacity, currJugs), new Jug(jugsCapacity, out));
+
+        while(it.hasNext()) {
+            AStar.State i = it.next();
             if(!it.hasNext()) {
                 assertEquals(String.format("%.0f", expected), String.format("%.0f", i.getG()));
             }
