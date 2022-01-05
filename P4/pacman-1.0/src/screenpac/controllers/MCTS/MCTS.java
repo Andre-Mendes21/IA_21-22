d9ec91eb9bfbef8b9cb7cd44b2cc56b7040b3ebe
Andre Mendes <andre.f.mendes24@gmail.com>
2022-01-05 16:05:31 +0000

    Implementation of MCTS 1.0v

--- P4/pacman-1.0/src/screenpac/controllers/MCTS/MCTS.java
+++ P4/pacman-1.0/src/screenpac/controllers/MCTS/MCTS.java
@@ -4,8 +4,8 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import screenpac.extract.Constants;
+import screenpac.features.Utilities;
 import screenpac.ghosts.GhostTeamController;
-import screenpac.model.GameState;
 import screenpac.model.GameStateInterface;
 
 public class MCTS implements Constants {
@@ -17,8 +17,10 @@ public class MCTS implements Constants {
     private final int numOfActivePillsStart;
     private final long timeDue;
 
-    public MCTS(GameStateInterface gameState, long timeDue) {
+    public MCTS(GameStateInterface gameState, GhostTeamController ghostsController, long timeDue) {
         this.rootNode = new MCTNode(gameState.copy(), 0);
+        this.ghostsController = ghostsController;
+
         this.numOfActivePillsStart = gameState.getNumberActivePills();
         this.timeDue = timeDue;
     }
@@ -27,11 +29,11 @@ public class MCTS implements Constants {
         long deltaTimeNS = 0;
         Long lastNS = System.nanoTime();
 
-        while(!Terminate(deltaTimeNS)) {
-            MCTNode selectedNode = TreePolicy(rootNode);
+        while(!terminate(deltaTimeNS)) {
+            MCTNode selectedNode = treePolicy(rootNode);
 
-            double reward = SimulateGame(selectedNode);
-            Backpropagate(selectedNode, reward);
+            double reward = simulateGame(selectedNode);
+            backpropagate(selectedNode, reward);
 
             long currentNS = System.nanoTime();
             deltaTimeNS = currentNS - lastNS;
@@ -45,30 +47,92 @@ public class MCTS implements Constants {
 
         return null;
     }
-
-    private boolean Terminate(long lastDeltaNS) {
+    
+    private boolean terminate(long lastDeltaNS) {
         long lastDeltaMillis = TimeUnit.MILLISECONDS.convert(lastDeltaNS, TimeUnit.NANOSECONDS);
         long returnTimeMS = 2;
         if(System.currentTimeMillis() + lastDeltaMillis + returnTimeMS > timeDue)
-            return true;
+        return true;
         return false;
     }
-
-    private MCTNode TreePolicy(MCTNode currentNode) {
+    
+    private MCTNode treePolicy(MCTNode currentNode) {
         if(currentNode.isGameOver()) {
             return currentNode.parent != null ? currentNode.parent : currentNode;
         }
-
+        
         if(!currentNode.isFullyExpanded()) {
             return expandNode(currentNode);
         }
-
+        
         if(currentNode.getChildren().isEmpty()) {
             return currentNode;
         }
         
-        boolean allChildVisits
+        return treePolicy(currentNode.getBestChild());
+    }
 
-        return TreePolicy(currentNode.getBestChild());
+    MCTNode expandNode(MCTNode parentNode) {
+        // FIXME: Change 140 magic number to Tree_Depth constant
+        ArrayList<DIR> pacmanMoves = parentNode.getPacmanMovesNotExpanded(140);
+        assert !pacmanMoves.isEmpty();
+        DIR pacmanDir = pacmanMoves.get(random.nextInt(pacmanMoves.size()));
+    
+        SimResult result = Utilities.simulateUntilNextJunction(parentNode.gameState.copy(), ghostsController, pacmanDir);
+    
+        MCTNode child = new MCTNode(result.gameState, parentNode.pathLengthInSteps + result.steps);
+    
+        child.parentAction = pacmanDir;
+        child.parent.parent = parentNode;
+    
+        parentNode.getChildren().add(child);
+    
+        if(result.didedDuringSim || result.powerPillUnncessarilyEaten) {
+            child.updateReward(-1);
+            child.setCanUpdate(false);
+    
+            return parentNode;
+        }
+    
+        return child;
+    }
+
+    // * May need to change in order to optimise Score
+    private double simulateGame(MCTNode selectedNode) {
+        GameStateInterface simGameState = selectedNode.gameState.copy();
+        // FIXME: Change 140 magic number to Tree_Depth constant
+        int totalSteps = 140 - selectedNode.pathLengthInSteps;
+        int remainingSteps = totalSteps;
+        SimResult lastSimResult = new SimResult();
+
+        while(!Utilities.analyzeGameState(simGameState, lastSimResult, remainingSteps, lastSimResult.powerPillUnncessarilyEaten)) {
+            ArrayList<DIR> availableMoves = Utilities.getPacmanMovesAtJunctionWithoutReverse(simGameState);
+            DIR pacmanDir = availableMoves.get(random.nextInt(availableMoves.size()));
+
+            lastSimResult = Utilities.simulateToNextJunctionOrLimit(simGameState, ghostsController, pacmanDir, remainingSteps);
+
+            remainingSteps -= lastSimResult.steps;
+        }
+
+        if(lastSimResult.levelComplete) {
+            return 1;
+        }
+
+        if(lastSimResult.didedDuringSim || lastSimResult.powerPillUnncessarilyEaten) {
+            return 0;
+        }
+
+        if(simGameState.getNumberActivePills() == numOfActivePillsStart) {
+            return 0.1d * (1.d / numOfActivePillsStart);
+        }
+
+        return 1.0d - (simGameState.getNumberActivePills() / (double) numOfActivePillsStart);
+    }
+
+    private void backpropagate(MCTNode selectedNode, double reward) {
+        while(selectedNode != null) {
+            selectedNode.updateReward(reward);
+            selectedNode = selectedNode.parent;
+        }
     }
 }
