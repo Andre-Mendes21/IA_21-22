1fb957625b0595c84bc4482c4b341c14cabb4384
Andre Mendes <andre.f.mendes24@gmail.com>
2022-01-05 21:05:24 +0000

    MCTS pacman agent controller implementation 1.00v

---
+++ P4/pacman-1.0/src/screenpac/controllers/MCTS/MCTSPacman.java
@@ -0,0 +1,32 @@
+package screenpac.controllers.MCTS;
+
+import screenpac.controllers.AgentInterface;
+import screenpac.controllers.MCTS.Utils.DIR;
+import screenpac.extract.Constants;
+import screenpac.ghosts.GhostTeamController;
+import screenpac.model.GameStateInterface;
+
+public class MCTSPacman implements AgentInterface, Constants{
+    private DIR myDir = DIR.NEUTRAL;
+    private GhostTeamController ghostsController;
+    private int timeDue;
+
+    public MCTSPacman(GhostTeamController ghostsController, int timeDue) {
+        this.ghostsController = ghostsController;
+        this.timeDue = timeDue;
+    }
+
+    @Override
+    public int action(GameStateInterface gs) {
+        MCTS mcts = new MCTS(gs, this.ghostsController, timeDue);
+        DIR nextDir = mcts.runMCTS();
+        System.out.println(nextDir.ordinal());
+
+        if(nextDir != null) {
+            myDir = nextDir;
+        }
+
+        return myDir.ordinal();
+    }
+    
+}
